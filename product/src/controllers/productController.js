84e4fbe5045dad109aa3703f990e6beb6a52782c
TaiTran <tantai2976@gmail.com>
2025-11-01 11:42:53 +0700

    test

--- product/src/controllers/productController.js
+++ product/src/controllers/productController.js
@@ -110,38 +110,9 @@ class ProductController {
       res.status(500).json({ message: "Server error" });
     }
   }
-  async getProductsById(req, res, next) {
-    try {
-      const token = req.headers.authorization;
-      if (!token) {
-        return res.status(401).json({ message: "Unauthorized" });
-      }
-      const id = req.params.id;
-      const product = await Product.findById(id);
-      res.status(200).json(product);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: "Server error" });
-    }
-  }
 
 
-  // async getProdctById(req, res, next){
-  //   try{
-  //     const token = req.headers.authorization;
-  //     if(!token){
-  //       return res.status(401).json({message: "Unauthorized"});
-  //     }
-  //     const {productId} = req.params;
-  //     const product = await Product.findById(productId);
-  //     res.status(200).json(product);
-  //   }
-  //   catch(error)
-  //   {
-  //     console.error(error);
-  //     res.status(500).json({message: "Server error"});
-  //   }
-  // }
+
 
 }
 
--- product/src/routes/productRoutes.js
+++ product/src/routes/productRoutes.js
@@ -4,10 +4,9 @@ const isAuthenticated = require("../utils/isAuthenticated");
 
 const router = express.Router();
 const productController = new ProductController();
-
+//"/:id"
 router.post("/", isAuthenticated, productController.createProduct);
 router.post("/buy", isAuthenticated, productController.createOrder);
 router.get("/", isAuthenticated, productController.getProducts);
-// /:productId
-router.get("/:id", isAuthenticated, productController.getProductsById);
+
 module.exports = router;
